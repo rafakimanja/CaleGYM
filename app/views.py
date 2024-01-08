0ebff5665d8be94886a6a2bd0ec7610ad1d66ffd
Rafakimanja <rafaellopes.ifrs@gmail.com>
2024-01-08 14:05:31 -0300

    new commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from app.models import DiasTreino
-from django.utils import timezone
 from app.calendario import Calendario
+from datetime import datetime
 
 
 # class Usuario:
@@ -19,21 +19,37 @@ from app.calendario import Calendario
 def index(request):
 
     meu_calendario = Calendario()
-
     datas = meu_calendario.calendario()
 
     treinos = DiasTreino.objects.all()
     ultimo_registro = treinos.last()
 
+    dias_treinados = DiasTreino.objects.filter(treino=True)
+
+    qtd_treinos = len(dias_treinados)
+
     if request.method == 'POST':
         escolha = request.POST.get('botao')
 
-        if escolha == 'sim':
-            dia_treino = DiasTreino(treino=True)
-            dia_treino.save()
+        if ultimo_registro is not None:
+
+            if ultimo_registro.registro.day != datetime.now().day:
+
+                if escolha == 'sim':
+                    dia_treino = DiasTreino(treino=True)
+                else:
+                    dia_treino = DiasTreino(treino=False)
+                
+                dia_treino.save()
         
         else:
-            dia_treino = DiasTreino(treino=False)
+            if escolha == 'sim':
+                dia_treino = DiasTreino(treino=True)
+            else:
+                dia_treino = DiasTreino(treino=False)
+        
             dia_treino.save()
 
-    return render(request, 'index.html', {'datas': datas, 'treinos': treinos})
+
+    return render(request, 'index.html', {'datas': datas, 'treinos': treinos, 'qtd_treinos': qtd_treinos})
+
